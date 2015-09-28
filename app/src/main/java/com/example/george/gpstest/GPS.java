f049a70dfe88a4eabf76203a5126db80b813174e
George <PanickedRhino@users.noreply.github.com>
2015-09-28 19:56:54 -0400

    commiting ayy lmao from command line

--- app/src/main/java/com/example/george/gpstest/GPS.java
+++ app/src/main/java/com/example/george/gpstest/GPS.java
@@ -15,6 +15,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 //Further testing git
+//sep 28 7:54 pm ayy
 public class GPS extends Activity implements LocationListener {
 
     public TextView out;
