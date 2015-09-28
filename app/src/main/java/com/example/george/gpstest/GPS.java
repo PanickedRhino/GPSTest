34940ce993cb09c2f26ec0a3158db4ee7fbf9e6e
George <PanickedRhino@users.noreply.github.com>
2015-09-28 19:30:52 -0400

    test1

--- app/src/main/java/com/example/george/gpstest/GPS.java
+++ app/src/main/java/com/example/george/gpstest/GPS.java
@@ -14,7 +14,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-//TOP KEK M8
+//Further testing git
 public class GPS extends Activity implements LocationListener {
 
     public TextView out;
